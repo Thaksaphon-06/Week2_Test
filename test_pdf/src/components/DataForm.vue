2426463f597626dbb2032cdc60d9019807531280
Touchhh <Mookthaksaphon@gmail.com>
2026-02-23 11:02:35 +0700

    update components

--- test_pdf/src/components/DataForm.vue
+++ test_pdf/src/components/DataForm.vue
@@ -254,7 +254,6 @@ const printPDF = async () => {
       title: 'กรุณากรอกข้อมูลให้ครบถ้วน',
       text: 'โปรดตรวจสอบข้อมูลในแบบฟอร์มอีกครั้ง',
       confirmButtonText: 'ตกลง',
-      confirmButtonColor: '#0D47A2',
     })
     return
   }
